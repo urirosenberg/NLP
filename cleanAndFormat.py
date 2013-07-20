73659339d223eca9095f932fa1f543c48f5f4916
urirosenberg <ur269a@intl.att.com>
2013-07-20 13:08:45 +0300

    clean and format

---
+++ cleanAndFormat.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import sys
+import xml.dom.minidom
+import re
+import nltk
+from nltk.corpus import stopwords
+
+
+"""
+    This is pre processing the doc
+"""
+
+doctlist = []
+fileOUT='outpu.txt'
+pattern=re.compile("[^\w']|_")
+stopwords = stopwords.words('english')
+
+
+def getText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+def handleTok(tokenlist):
+    texts = ""
+    for token in tokenlist:
+        texts += " "+ getText(token.childNodes)
+    return texts
+
+
+
+if len(sys.argv) != 2:
+    print 'Usage: p1.py [path]filename'
+    sys.exit()
+
+fileIN = sys.argv[1]
+f=open(fileIN,'r')
+lineNumber=0
+
+for rawDoc in f.readlines():
+   lineNumber+=1
+   rawDoc = '<root>' + rawDoc + '</root>'
+   dom = xml.dom.minidom.parseString(rawDoc)
+   
+   #get the subject
+   subjectElement = dom.getElementsByTagName("subject")
+   subject = handleTok(subjectElement)
+   
+   #get category
+   categoryElement=dom.getElementsByTagName("maincat")
+   category = handleTok(categoryElement)
+
+   #get content
+   contentElement=dom.getElementsByTagName("content")
+   content = handleTok(contentElement)
+   
+   allText=subject+ ' ' + content
+   allText=pattern.sub(' ', allText)
+   allText=' '.join(allText.split())
+   allText= allText.lower()
+   filtered_words = [w for w in str(allText).split() if w not in stopwords]
+
+   doctlist.append({'text': str(filtered_words), 'category': str(category)})
+   if lineNumber % 1000 == 0:
+      print "processed 1000 lines"
+
+fout=open(fileOUT,'w')
+for item in doctlist:
+  fout.write("%s\n" % item)
+fout.close()
